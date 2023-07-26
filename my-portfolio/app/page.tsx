0aaf9f94a1d0cda0d078b07d3117e138e48238d6
JacobJJoness <genjacob20@gmail.com>
2023-07-26 04:48:51 -0500

    Updated Logo and Resume Page
    Resume page is now much closer to formatted

--- my-portfolio/app/page.tsx
+++ my-portfolio/app/page.tsx
@@ -1,6 +1,8 @@
 import Image from 'next/image';
 import Link from 'next/link';
 
+
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
@@ -24,7 +26,7 @@ export default function Home() {
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-pink-700 before:dark:opacity-10 after:dark:from-pink-900 after:dark:via-[#ff1a01] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
         <Image
           className="relative dark:drop-shadow-[0_0_0.35rem_#ff0303] "
-          src="/jones.png"
+          src="/jjj.png"
           alt="JJ Logo"
           width={400}
           height={350}
