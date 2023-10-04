b76aefc040596704c4675e4e0a461b21ee61f829
JacobJJoness <genjacob20@gmail.com>
2023-10-03 20:29:51 -0500

    Removed Duplicate Dependancy
    Removed a dependancy react-tsparticles as it was the same library as react-particles

--- my-portfolio/components/ProjectParticles.tsx
+++ my-portfolio/components/ProjectParticles.tsx
@@ -3,7 +3,7 @@ import Particles from "react-particles";
 import type { Container, Engine } from "tsparticles-engine";
 //import { loadFull } from "tsparticles"; // if you are going to use `loadFull`, install the "tsparticles" package too.
 import { loadSlim } from "tsparticles-slim"; // if you are going to use `loadSlim`, install the "tsparticles-slim" package too.;
-import HomeParticles from '../config/HomeParticles';
+
 
 const ProjectParticles = () => {
   const particlesInit = useCallback(async(engine: Engine) => {
